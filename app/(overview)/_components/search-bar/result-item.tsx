4f06e07a34ea1a6b512df1a170b23c774d499f57
shehab ayman <shehabayman355@gmail.com>
2025-08-26 16:13:55 +0300

    deploy: update deployment

--- app/(overview)/_components/search-bar/result-item.tsx
+++ app/(overview)/_components/search-bar/result-item.tsx
@@ -7,7 +7,10 @@ type TResultItem = {
 export const ResultItem = ({ description, count, amount }: TResultItem) => {
     return (
         <div className="flex items-center justify-between rounded-md bg-gradient-to-b from-amber-500 to-amber-700 px-4 py-2 font-semibold text-white">
-            <p>{description}</p> : <p>{count} PCS</p> : <p>{amount} $</p>
+            <p>{description}</p>
+            <div className="flex justify-end gap-2">
+                : <p>{count} PCS</p> : <p>{amount} $</p>
+            </div>
         </div>
     );
 };

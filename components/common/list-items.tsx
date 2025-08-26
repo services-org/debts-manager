1fee92e39f881e5b1e937165677590e919be2eda
shehab ayman <shehabayman355@gmail.com>
2025-08-26 15:42:52 +0300

    deploy: update deployment

--- components/common/list-items.tsx
+++ components/common/list-items.tsx
@@ -8,14 +8,13 @@ type TListItems<TData> = {
     props?: Record<string, any>;
     emptyMessage?: string;
     isPending?: boolean;
-    key?: keyof TData;
 };
 
-export const ListItems = <TData,>({ key, Item, props, items = [] }: TListItems<TData>) => {
+export const ListItems = <TData,>({ Item, props, items = [] }: TListItems<TData>) => {
     if (!Array.isArray(items)) return;
 
-    // @ts-ignore
-    return items?.map((item, index) => <Item key={item._id ?? item?.[key] ?? index} index={index} {...item} {...props} />);
+    // @ts-expect-error TS2322
+    return items?.map((item, index) => <Item key={item._id ?? index} index={index} {...item} {...props} />);
 };
 
 ListItems.displayName = "ListItems";
